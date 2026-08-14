4e22bf36057e3f5fe9929383441a3f716c2f6c23
Adeife79 <ifeoluwaaderoju6@gmail.com>
2026-08-14 09:44:07 +0100

    Modify eks.tf

--- AWS-Infrastructure/eks.tf
+++ AWS-Infrastructure/eks.tf
@@ -16,7 +16,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 
 # EKS Node Group
 resource "aws_eks_node_group" "eks_node" {
-  #  cluster_name = aws_eks_cluster.eks_cluster.name
+    cluster_name = aws_eks_cluster.eks_cluster.name
 
     node_group_name = var.node_group_name
 
